3d2ba06126cd9ced516ed5b8af9f02e10657b37b
hugogit120 <hugomercalibre@gmail.com>
2020-11-14 18:31:52 +0100

    first commit on main-branch

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,11 @@
 import Axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 const Main = () => {
+
+
+
     return(
         <h1>estas en main!</h1> 
     )
